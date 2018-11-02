0bc444d42d4533ccbedf4f422650e5c2d5d2d8cc
Joel Capitao <joel@capitao.fr>
2018-11-02 10:24:54 +0100

    add blog tmuxifier layout

--- tmuxifier_layouts/editor.window.sh
+++ tmuxifier_layouts/editor.window.sh
@@ -1,9 +1,5 @@
-# Set window root path. Default is `$session_root`.
-# Must be called before `new_window`.
-
-# Create new window. If no argument is given, window name will be based on
-# layout file name.
-
 new_window "editor"
-
 run_cmd "vim ."
+split_v 10
+run_cmd "git s"
+select_pane 1
